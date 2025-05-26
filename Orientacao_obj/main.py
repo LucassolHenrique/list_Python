2b11594968fbb31324050c4b5ef2847568d3cbbe
Lucas sol <porquenpode@gmail.com>
2025-05-26 09:51:46 -0300

    Commit_

---
+++ Orientacao_obj/main.py
@@ -0,0 +1,45 @@
+# Você deverá criar um sistema de locker de entrega de produtos.
+# Lógica: O entregador realiza a entrega de um produto escolhendo a opção ENTREGA. 
+#               Informa o tamanho do pacote, o apartamento e finaliza a entrega. O sistema envia uma mensagem ao nome do apartamento cadastrado com uma senha gerada aleatóriamente.
+#
+#.             O morador, ao retirar o produto, informa o apartamento e a senha, o locker "Abre". O morador Finaliza a retirada e o locker é liberado.
+#
+#.             Em configurações deve existir uma opção de cadastro, onde o usuário irá cadastrar o apartamento e uma senha para utilizar o locker.
+
+from Objetos import Morador
+
+def menu():
+  print("\nMenu:")
+  print("1 - Sair do aplicativo")
+  print("2 - Realizar Entrega")
+  print("3 - Status do Locker")
+  print("4 - Status do Apartamento")
+  print("5 - Cadastro de novo apartamento, morador e locker")
+  print("6 - Retirar produto")
+
+
+# Menu principal
+while True:
+  menu()
+  entrada = -1
+  while entrada <= 1 or entrada >= 7:
+      try:
+          entrada = int(input("Escolha uma opção: "))
+          break
+      except ValueError:
+          print("Digite uma opção válida.")
+
+  if entrada == 1:
+      print("Saindo do aplicativo...")
+      break
+  elif entrada == 2:
+      print(Morador.mostrar_dados(f"1, João"))
+      
+  # elif entrada == 3:
+  #     status_locker()
+  # elif entrada == 4:
+  #     status_apartamento()
+  # elif entrada == 5:
+  #     criapredio()
+  # elif entrada == 6:
+  #     retirar_produto()
--- Projeto_locker/Locker_system/class_SystemaLocker.py
+++ Projeto_locker/Locker_system/class_SystemaLocker.py
@@ -3,13 +3,6 @@ from Aula08_25.class_locker import *
 from Aula08_25.class_morador import *
 
 class SistemaLocker:
-    """
-    Classe principal do sistema de lockers, gerenciando moradores e lockers.
-
-    Atributos:
-        moradores (dict): Dicionário que mapeia IDs para objetos `Morador`.
-        lockers (dict): Dicionário que mapeia IDs para objetos `Locker`.
-    """
 
     def __init__(self) -> None:
         """Inicializa o sistema com dicionários de moradores e lockers."""
@@ -17,24 +10,11 @@ class SistemaLocker:
         self.__lockers = {}
 
     def apartamento_valido(self, apartamento: str) -> bool:
-        """
-        Verifica se o apartamento está registrado no sistema.
 
-        Args:
-            apartamento (str): Número do apartamento.
-
-        Returns:
-            bool: True se o apartamento for válido, False caso contrário.
-        """
         return any(morador.apt == apartamento for morador in self.__moradores.values())
 
     def existe_algum_locker_disponivel(self) -> bool:
-        """
-        Verifica se há lockers disponíveis.
 
-        Returns:
-            bool: True se existir ao menos um locker disponível, False caso contrário.
-        """
         if any(locker.esta_disponivel() for locker in self.__lockers.values()):
             return True
         input("OPS. Não há lockers disponíveis. Pressione Enter.")
@@ -48,16 +28,7 @@ class SistemaLocker:
                 print(f"[{locker.id}] - {locker.tamanho}")
 
     def locker_disponivel(self, tamanho: str) -> Locker | None:
-        """
-        Retorna um locker disponível com o tamanho desejado.
 
-        Args:
-            tamanho (str): Tamanho do locker desejado ('P', 'M', 'G').
-
-        Returns:
-            Locker: Um locker disponível para uso.
-            None: Se não houver lockers disponíveis.
-        """
         for locker in self.__lockers.values():
             if locker.esta_disponivel() and locker.tamanho_certo(tamanho):
                 print(f"Locker {locker.id} disponível.")
@@ -74,13 +45,7 @@ class SistemaLocker:
         self.cadastrar_locker(5, Locker(5, "G"))
 
     def cadastrar_locker(self, id_: int, locker: Locker) -> None:
-        """
-        Registra um locker no sistema.
 
-        Args:
-            id_ (int): ID do locker.
-            locker (Locker): Objeto locker a ser registrado.
-        """
         self.__lockers[id_] = locker
         print(f"Locker {locker.id} cadastrado com sucesso!")
 
@@ -90,26 +55,12 @@ class SistemaLocker:
         self.cadastrar_morador(3, Morador("Pedro", "301", "3333"))
 
     def cadastrar_morador(self, id_: int, novo_morador: Morador) -> None:
-        """
-        Registra um morador no sistema.
 
-        Args:
-            id_ (int): ID do morador.
-            novo_morador (Morador): Objeto morador a ser registrado.
-        """
         self.__moradores[id_] = novo_morador
         print(f"Apartamento {novo_morador.apt} cadastrado com sucesso!")
 
     def retirar_pacote(self, senha_informada: str) -> None:
-        """
-        Permite a retirada do pacote se a senha informada for correta.
 
-        Args:
-            senha_informada (str): Senha fornecida pelo morador para abrir o locker.
-
-        Returns:
-            None
-        """
         for locker in self.__lockers.values():
             if locker.senha_random == senha_informada:
                 locker.disponibilizar()
