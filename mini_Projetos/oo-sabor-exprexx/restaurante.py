dcc220a6e356e1270c670996ad7224be36fb5a22
Lucas sol <porquenpode@gmail.com>
2025-05-29 10:00:22 -0300

    ATT

---
+++ mini_Projetos/oo-sabor-exprexx/restaurante.py
@@ -0,0 +1,17 @@
+
+class Restaurante: 
+    def __init__(self, nome, categoria, ativo):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False       #
+
+    def __str__(self):
+        self.nome
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italian pizza')
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
